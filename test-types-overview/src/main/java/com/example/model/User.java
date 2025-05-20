c69519e6b90fb71425325a5a80c61cbbfbf89ed8
CITGLOBAL2012\anabil <anabil@citglobal.com>
2025-05-20 10:19:31 +0300

    added coverage branch missed sample

---
+++ test-types-overview/src/main/java/com/example/model/User.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+}
