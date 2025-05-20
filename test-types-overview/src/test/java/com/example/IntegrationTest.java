f757c1573df6a0848fc24038a63870ad875d8c1b
CITGLOBAL2012\anabil <anabil@citglobal.com>
2025-05-20 08:55:56 +0300

    Added DataJpaTest H2DB sample

--- test-types-overview/src/test/java/com/example/IntegrationTest.java
+++ test-types-overview/src/test/java/com/example/IntegrationTest.java
@@ -1,17 +1,29 @@
 package com.example;
 
+import com.example.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class IntegrationTest {
     @Autowired
-    private Repository repository;
+    private UserRepository repository;
 
     @Test
     void testSave() {
-        User user = repository.save(new User(1, "Wim"));
+        User user = User.builder().firstName("Alice").build();
+
+        User savedUser = repository.save(user);
+
+
         assertThat(user).isNotNull();
+        Assertions.assertNotNull(savedUser.getId());
     }
 }
